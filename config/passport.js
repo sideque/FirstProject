af72a7531e8f1d9c0a720cb6fcba43e7ad9bf4de
Sidhique <xidhique@gmail.com>
2025-05-12 21:47:56 +0530

    http

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env = require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://mobivault.shop/auth/google/callback'
+    callbackURL: 'http://mobivault.shop/auth/google/callback'
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
